bf18c12f2934f08ff3fcd5ee17e123e42b51aedb
Hrothgar McClendon <nickmcclendon@gmail.com>
2011-08-01 21:36:22 -1000

    fixed up post-processing scripts

--- dirtywork.py
+++ dirtywork.py
@@ -1,5 +1,5 @@
 file_forcesdat = open('forces/0/forces.dat','r')
-tt, ff = [], []
+tt, ff, aa = [], [], []
 for indx,line in enumerate(file_forcesdat):
 	if indx > 0:
           	line = [float(i) for i in line.replace('(','').replace(')','').split()]
@@ -7,7 +7,13 @@ for indx,line in enumerate(file_forcesdat):
 		f    = line[1] + line[4] # directional along the x-axis, sum of pressure and viscous forces
 		tt  += [t]
 		ff  += [f]
+for line in open('ff_angle','r'):
+	a   = float(line.split()[3]) * 180.0 / 3.141592653589
+	aa += [a]
+
 file_fx = open('ff_x','w+')
 file_ft = open('ff_time','w+')
+file_fa = open('ff_angle','w+')
 file_fx.write('\n'.join([str(i) for i in ff]))
 file_ft.write('\n'.join([str(i) for i in tt]))
+file_fa.write('\n'.join([str(i) for i in aa]))
--- plots.m
+++ plots.m
@@ -6,10 +6,22 @@ fid = fopen('ff_time');
 tt = fscanf(fid, '%f\n');
 fclose(fid);
 
+fid = fopen('ff_angle');
+aa = fscanf(fid, '%f\n');
+fclose(fid);
+
 plot(tt,ff,'k-','linewidth',2)
 xlabel('time (seconds)','fontsize',15)
 ylabel('force (Newtons)','fontsize',15)
 title('Forces on a rigid wall using interDyMFoam','fontsize',15)
 axis([0 10 0 475])
 
-print -dpng 'I:\Rigid wall results\1-processor\rigid-wall-interDyMFoam\ff_interDyMFoam.png'
+print -dpng 'I:\fw\ff_forces.png'
+
+plot(tt,aa,'r-','linewidth',2)
+xlabel('time (seconds)','fontsize',15)
+ylabel('angle (degrees)','fontsize',15)
+title('Angle of a flexible wall','fontsize',15)
+axis([0 10 0 10])
+
+print -dpng 'I:\fw\ff_angle.png'
