4c2f8a41de37cbcdf88d176b43ecd6029ab3ed62
Hrothgar McClendon <nickmcclendon@gmail.com>
2011-08-01 15:54:38 -1000

    adding plots.m, fixed up parseForces.py

--- parseForces.py
+++ parseForces.py
@@ -1,15 +1,13 @@
-f = open('forces/0/forces.dat','r')
-for indx,line in enumerate(f):
+file_forcesdat = open('forces/0/forces.dat','r')
+tt, ff = [], []
+for indx,line in enumerate(file_forcesdat):
 	if indx > 0:
-		line = line.replace('(','').replace(')','')
-		line = line.split()
-		line = line[1:7]
-		line = [float(i) for i in line]
-		magf = ( (line[0]+line[3]) + (line[1]+line[4]) + (line[2]+line[5]) )**(0.5)
-		xf   = line[0] + line[3]
-		fm   = open('forces_mag','a+')
-		fx   = open('forces_x','a+')
-		magfstr = str(magf) + ' '
-		xfstr   = str(xf) + ' '
-		fm.write(magfstr)
-		fx.write(xfstr)
+          	line = [float(i) for i in line.replace('(','').replace(')','').split()]
+		t    = line[0]
+		f    = line[1] + line[4] # directional along the x-axis, sum of pressure and viscous forces
+		tt  += [t]
+		ff  += [f]
+file_fx = open('ff_x','w+')
+file_ft = open('ff_time','w+')
+file_fx.write('\n'.join([str(i) for i in ff]))
+file_ft.write('\n'.join([str(i) for i in tt]))
